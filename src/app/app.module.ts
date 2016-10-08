e0cbaa86849e95cbe8008623da76e7963dab60fa
Jap9 <jap9@alumnes.udl.cat>
2016-10-08 09:44:14 +0200

    Added message components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PictureComponent } from './picture/picture.component';
 import { routing, appRoutingProviders }  from './app.routing';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IntroComponent } from './intro/intro.component';
+import { MessageComponent } from './message/message.component';
 import { AdvertisementComponent } from './advertisement/advertisement.component';
 import { PostAdvertisementComponent } from './advertisement/postAdvertisement.component';
 import { GetAdvertisementComponent } from './advertisement/getAdvertisement.component';
@@ -29,6 +30,8 @@ import { ContactComponent } from './contact/contact.component';
     PictureComponent,
     NavbarComponent,
     IntroComponent,
+    MessageComponent,
+    IntroComponent,
     AdvertisementComponent,
     PostAdvertisementComponent,
     GetAdvertisementComponent,
@@ -42,6 +45,6 @@ import { ContactComponent } from './contact/contact.component';
     appRoutingProviders,
     { provide: APP_BASE_HREF, useValue : '/' }
   ],
-  bootstrap: [AppComponent, NavbarComponent, FooterComponent]
+  bootstrap: [AppComponent, NavbarComponent]
 })
 export class AppModule { }

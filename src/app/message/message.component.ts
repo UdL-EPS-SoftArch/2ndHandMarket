ce427b36a877d4b68a190027f4c6f8fb2c0db0f0
Jap9 <jap9@alumnes.udl.cat>
2016-10-11 21:14:02 +0200

    Added some HTML design to the Messages Section

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {Message} from "./message";
+import {MessageService} from "./message.service";
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  styleUrls: ['./message.component.css'],
+  providers: [MessageService]
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  messages: Message[] = [];
+  errorMessage: string;
 
-  ngOnInit() {
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit() { this.getMessages(); }
+
+  getMessages() {
+    return this.messageService.getAllMessages()
+      .subscribe(
+        messages => this.messages = messages,
+        error =>  this.errorMessage = <any>error.message);
   }
 
 }

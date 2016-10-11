6503731f0f60469bec991c9fc8b5bfc20ee0aa49
Jap9 <jap9@alumnes.udl.cat>
2016-10-11 21:14:02 +0200

    Added some HTML design to the Messages Section

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
+import {Message} from "./message";
+import {Observable} from "rxjs";
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class MessageService {
 
-  constructor() { }
+  constructor (private http: Http) {}
+
+  // GET /pictures
+  getAllMessages(): Observable<Message[]> {
+    return this.http.get(`${environment.API}/messages`)
+      .map((res: Response) => res.json().getAllMessages())//?
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
 }

fce75f99d89efacb1b5b272b2362a5efa33bf81a
Jap9 <jap9@alumnes.udl.cat>
2016-10-12 16:08:08 +0200

    Added GET, GET by URI and POST methods on message.service

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http, Headers, RequestOptions, Response} from '@angular/http';
 import {Message} from "./message";
 import {Observable} from "rxjs";
@@ -9,10 +9,29 @@ export class MessageService {
 
   constructor (private http: Http) {}
 
-  // GET /pictures
+  // GET /messages
   getAllMessages(): Observable<Message[]> {
     return this.http.get(`${environment.API}/messages`)
-      .map((res: Response) => res.json().getAllMessages())//?
+      .map((res: Response) => res.json().getAllMessages())//?????
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // GET /messages/:id
+  getMessageByUri(uri: string): Observable<Message> {
+    return this.http.get(`${environment.API}${uri}`)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // POST /messages
+  addMessage(message: Message): Observable<Message> {
+    let body = JSON.stringify({ 'title': message.title, 'body': message.body});
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', 'Basic ' + btoa(environment.user + ':' + environment.password));
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(`${environment.API}/messages`, body, options)
+      .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json()));
   }
 

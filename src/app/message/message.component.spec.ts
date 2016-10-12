72a341d7aa9d3c4661bf97b004e48e8cc53deded
Jap9 <jap9@alumnes.udl.cat>
2016-10-12 16:08:08 +0200

    Added GET, GET by URI and POST methods on message.service

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -1,11 +1,22 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { MessageComponent } from './message.component';
+import {TestBed, async, inject} from '@angular/core/testing';
+import {MessageComponent } from './message.component';
+import {MessageService } from './message.service';
 
 describe('Component: Message', () => {
-  it('should create an instance', () => {
-    let component = new MessageComponent();
-    expect(component).toBeTruthy();
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MessageService]
+    });
   });
+
+  it('should create an instance', () => {
+    inject([MessageService], (messageService) => {
+      let component = new MessageComponent(messageService);
+      expect(component).toBeTruthy();
+    });
+  });
+
 });

31c913939c620d568365c021a7b6e19139ea199a
Jap9 <jap9@alumnes.udl.cat>
2016-10-14 23:34:21 +0200

    Added DELETE method on message.service and fixed UI issues on message.component.HTML

--- src/app/message/message.ts
+++ src/app/message/message.ts
@@ -2,6 +2,7 @@ export class Message {
   uri: string = '';
   title: string = '';
   body: string = '';
+  destination: string = '';
 
   constructor(values: Object = {}) {
     Object.assign(this, values);

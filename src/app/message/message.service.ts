bc7e770914586f801d5d770b276cf23e2f8e19f4
Jap9 <jap9@alumnes.udl.cat>
2016-10-14 23:34:21 +0200

    Added DELETE method on message.service and fixed UI issues on message.component.HTML

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -9,29 +9,39 @@ export class MessageService {
 
   constructor (private http: Http) {}
 
-  // GET /messages
+  // GET /privateMessages
   getAllMessages(): Observable<Message[]> {
     return this.http.get(`${environment.API}/privateMessages`)
       .map((res: Response) => res.json().getAllMessages())//?????
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  // GET /messages/:id
+  // GET /privateMessages/:id
   getMessageByUri(uri: string): Observable<Message> {
     return this.http.get(`${environment.API}${uri}`)
       .map((res: Response) => res.json())
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  // POST /messages
+  // POST /privateMessages
   addMessage(message: Message): Observable<Message> {
-    let body = JSON.stringify({ 'title': message.title, 'body': message.body});
+    let body = JSON.stringify({ 'title': message.title, 'body': message.body, 'destination' : message.destination});
     let headers = new Headers({ 'Content-Type': 'application/json' });
     headers.append('Authorization', 'Basic ' + btoa(environment.user + ':' + environment.password));
     let options = new RequestOptions({ headers: headers });
 
     return this.http.post(`${environment.API}/privateMessages`, body, options)
       .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // DELETE /privateMessages/:id
+  deleteMessageByUri(uri: string) {
+    let headers = new Headers({ 'Authorization': 'Basic ' + btoa(environment.user + ':' + environment.password) });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.delete(`${environment.API}${uri}`, options)
+      .map((res: Response) => res.ok)
       .catch((error: any) => Observable.throw(error.json()));
   }
 

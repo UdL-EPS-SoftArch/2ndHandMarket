09e50cd54d18de675e389d6f1ffd730051c59625
Jap9 <jap9@alumnes.udl.cat>
2016-10-23 13:32:54 +0200

    Added "sender" component on HTML by getting logged user and fix DELETE method

--- src/app/message/message.ts
+++ src/app/message/message.ts
@@ -1,8 +1,10 @@
+import {environment} from "../../environments/environment";
 export class Message {
   uri: string = '';
   title: string = '';
   body: string = '';
   destination: string = '';
+  sender: string = environment.user;
 
   constructor(values: Object = {}) {
     Object.assign(this, values);

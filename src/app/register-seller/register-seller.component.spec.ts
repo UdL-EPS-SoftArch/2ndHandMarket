21963c0b15f9e2c035c95a7dbb2f5d5bfd58a990
carles <carlessaez5@gmail.com>
2016-10-26 16:18:44 +0200

    Fixed some bugs

--- src/app/register-seller/register-seller.component.spec.ts
+++ src/app/register-seller/register-seller.component.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 import { RegisterSellerComponent} from './register-seller.component';
 import {RegisterSellerService} from "./register-seller.service";
 

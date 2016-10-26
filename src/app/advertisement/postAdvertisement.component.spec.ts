15b2ce543c64a00406310dbf84da83b29ecf35e1
Roberto García <roberto@rhizomik.net>
2016-10-26 16:13:04 +0200

    Fixed failing tests

--- src/app/advertisement/postAdvertisement.component.spec.ts
+++ src/app/advertisement/postAdvertisement.component.spec.ts
@@ -3,17 +3,18 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PostAdvertisementComponent } from './postAdvertisement.component';
 import { AdvertisementService } from './advertisement.service';
+import { Router } from '@angular/router';
 
 describe('Component: PostAdvertisement', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ AdvertisementService ]
+      providers: [ Router, AdvertisementService ]
     });
   });
 
   it ('should create an instance', () => {
-    inject([ AdvertisementService ], (advertisementService) => {
-      let component = new PostAdvertisementComponent(advertisementService);
+    inject([ Router, AdvertisementService ], (router, advertisementService) => {
+      let component = new PostAdvertisementComponent(router, advertisementService);
       expect(component).toBeTruthy();
     });
   });

293f4f6696afe9a3b48f52e016c40535d84dbbed
Jap9 <jap9@alumnes.udl.cat>
2016-10-26 16:52:02 +0200

    Added Search by Uri feature (need some work to do)

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,24 +1,28 @@
 import {Component, OnInit } from '@angular/core';
-import {Message} from "./message";
-import {MessageService} from "./message.service";
+import {Message} from './message';
+import {MessageService} from './message.service';
+import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css'],
-  providers: [MessageService]
+  providers: [MessageService, AuthenticationBasicService]
 })
 export class MessageComponent implements OnInit {
 
   messages: Message[] = [];
   errorMessage: string;
-  newMessage: Message = new Message();
-  messagesUri: Message[] = [];
-  messagesTitle: Message[] = [];
+  newMessage: Message;
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService,
+              private authentication: AuthenticationBasicService) { }
 
-  ngOnInit() { this.getMessages(); }
+  ngOnInit() {
+    this.getMessages();
+    this.newMessage = new Message();
+    this.newMessage.sender = this.authentication.getCurrentUser().username;
+  }
 
   getMessages() {
     return this.messageService.getAllMessages()
@@ -30,23 +34,17 @@ export class MessageComponent implements OnInit {
   getMessageByUri(uri) {
     return this.messageService.getMessageByUri(uri)
       .subscribe(
-        message => this.messagesUri = [message],
-        error =>  this.errorMessage = <any>error.message);
-  }
-
-  getMessageByTitle (title) {
-    return this.messageService.getAllMessages()
-      .subscribe(
-        message => this.messagesTitle = this.messages.filter(p => p.title == title),
+        message => this.messages = [message],
         error =>  this.errorMessage = <any>error.message);
   }
 
   addMessage() {
-      this.messageService.addMessage(this.newMessage)
-        .subscribe(
-          message  => this.messages.push(message),
-          error =>  this.errorMessage = <any>error.message);
-      this.newMessage = new Message();
+    this.messageService.addMessage(this.newMessage)
+      .subscribe(
+        message  => this.messages.push(message),
+        error =>  this.errorMessage = <any>error.message);
+    this.newMessage = new Message();
+    this.newMessage.sender = this.authentication.getCurrentUser().username;
   }
 
   removeMessage(message) {

58d26f0462e1530573d0db9af62fbb8d17f796ad
Roberto García <roberto@rhizomik.net>
2016-10-28 22:27:00 +0200

    Fix typings error for require NodeRequire

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,4 +3,16 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
-declare var require: any;
+
+interface NodeRequireFunction {
+  (id: string): any;
+}
+
+interface NodeRequire extends NodeRequireFunction {
+  resolve(id: string): string;
+  cache: any;
+  extensions: any;
+  main: any;
+}
+
+declare var require: NodeRequire;

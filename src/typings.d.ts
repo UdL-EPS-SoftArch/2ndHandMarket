1f7329309b83258ea13ba36a6898bfe1bd9e8d64
Roberto García <roberto@rhizomik.net>
2016-11-13 09:56:40 +0100

    Migrate to angular-cli-b19-3 and angular 2.1.0

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,18 +1,4 @@
-// Typings reference file, see links for more information
-// https://github.com/typings/typings
+// Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
-
-interface NodeRequireFunction {
-  (id: string): any;
-}
-
-interface NodeRequire extends NodeRequireFunction {
-  resolve(id: string): string;
-  cache: any;
-  extensions: any;
-  main: any;
-}
-
-declare var require: NodeRequire;

d80c8a99f13aef4fe58d60688860cbe58f2f7cf5
jennifer <jsc14@alumnes.udl.cat>
2016-11-30 16:59:18 +0100

    Seller component

---
+++ src/app/seller/seller.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller',
+  templateUrl: './seller.component.html',
+  styleUrls: ['./seller.component.css']
+})
+export class SellerComponent implements OnInit {
+
+
+  constructor() {
+    this.global = 1;
+  }
+
+  global : number;
+
+
+  ngOnInit() {
+  }
+
+  ShowProducts(): void{
+    this.global = 1;
+
+  }
+
+  ShowCountOffers() : void{
+    this.global = 2;
+
+  }
+
+  AddCoupons() : void{
+    this.global = 3;
+
+  }
+
+}

d71e3ddcb2ea8aeb46fdceb46f5e6e282cea9e93
srblimp <srblimp@gmail.com>
2016-12-01 16:26:24 +0100

    Updated functionality search

--- src/app/advertisement/search-advertisement/search-advertisement.component.ts
+++ src/app/advertisement/search-advertisement/search-advertisement.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Advertisement} from '../advertisement';
+import {SearchAdvertisementService} from '../search-advertisement/searchAdvertisement.service';
+
 @Component({
   selector: 'search-ad',
   templateUrl: './search-advertisement.component.html',
-  styleUrls: ['search-advertisement.component.scss']
+  styleUrls: ['search-advertisement.component.scss'],
+  providers: [SearchAdvertisementService]
 })
 export class SearchAdvertisementComponent implements OnInit {
 
-  constructor() { }
+  advertisements: Advertisement[] = [];
+  searchInput: string = '';
 
-  ngOnInit() {
+  constructor(private searchAdvertisementService:SearchAdvertisementService) {
   }
 
+  ngOnInit() {
+    this.searchAdvertisementByTitle();
+    this.searchAdvertisementByTag();
+    this.searchAdvertisementByCategory();
+  }
+
+  searchAdvertisementByTitle() {
+    this.searchAdvertisementService.searchAdvertisementByTitle(this.searchInput).subscribe(
+      advertisements => this.advertisements = advertisements,
+      error => alert('Error: Failed to retrieve advertisement!')
+    );
+  }
+
+  searchAdvertisementByTag() {
+    this.searchAdvertisementService.searchAdvertisementByTag(this.searchInput).subscribe(
+      advertisements => this.advertisements = advertisements,
+      error => alert('Error: Failed to retrieve advertisement!')
+    );
+  }
+
+  searchAdvertisementByCategory() {
+    this.searchAdvertisementService.searchAdvertisementByCategory(this.searchInput).subscribe(
+      advertisements => this.advertisements = advertisements,
+      error => alert('Error: Failed to retrieve advertisement!')
+    );
+  }
 }

bda6b707d5ac2ceb87639265d804c0876064058d
xavier <xplanes123@gmail.com>
2016-12-07 17:09:35 +0100

    Advertisement linked to offer

--- src/app/buyeroffer/buyeroffer.component.ts
+++ src/app/buyeroffer/buyeroffer.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, Input} from '@angular/core';
 import { BuyerOffer } from './buyeroffer';
 import { BuyerOfferService } from './buyeroffer.service';
+import {EventEmitter} from "@angular/common/src/facade/async";
 
 
 @Component({
@@ -15,10 +16,14 @@ export class BuyerOfferComponent implements OnInit {
   errorMessage: string;
   newBuyerOffer: BuyerOffer = new BuyerOffer();
 
+  //@Output() retrieveAdvertisement:EventEmitter<any> = new EventEmitter();
+  @Input('advertisement') advertisement;
+
   constructor(private buyerofferService: BuyerOfferService) { }
 
   ngOnInit() {
     this.getBuyerOffer();
+    console.info(this.advertisement);
   }
 
   getBuyerOffer() {
@@ -29,8 +34,9 @@ export class BuyerOfferComponent implements OnInit {
       );
   }
 
-  addBuyerOffer(/*advertisement_id : number*/) {
-    //this.newBuyerOffer.advertisement_id = advertisement_id;
+  addBuyerOffer() {
+    this.newBuyerOffer.advertisement_id = this.advertisement.id;
+    console.info(this.newBuyerOffer);
     this.buyerofferService.addBuyerOffer(this.newBuyerOffer)
       .subscribe(
         buyeroffer  => this.buyeroffers.push(buyeroffer),
@@ -44,5 +50,4 @@ export class BuyerOfferComponent implements OnInit {
         deleted => this.buyeroffers = this.buyeroffers.filter(p => p.uri !== buyeroffer.uri),
         error =>  this.errorMessage = <any>error.message);
   }
-
 }

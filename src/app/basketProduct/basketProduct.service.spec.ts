b6c8f73e6ffbe4062ba7fc8a880b050b32aa2be1
Roberto García <roberto@rhizomik.net>
2016-12-08 23:09:47 +0100

    BasketProductService client side mode

--- src/app/basketProduct/basketProduct.service.spec.ts
+++ src/app/basketProduct/basketProduct.service.spec.ts
@@ -4,56 +4,17 @@ import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions, XHRBackend, Http, HttpModule, ResponseOptions, Response } from '@angular/http';
 import { AuthenticationBasicService } from '../login-basic/authentication-basic.service';
 
-class ResponseError extends Error {
-  json() {
-    return '{ \'message\': \'Error\' }';
-  }
-}
+
 
 describe('Service: BasketProduct', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       providers: [
-        BasketProductService,
-        AuthenticationBasicService,
-        MockBackend,
-        BaseRequestOptions,
-        {
-          provide: Http,
-          deps: [MockBackend, BaseRequestOptions],
-          useFactory: (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
-            return new Http(backend, defaultOptions);
-          },
-        }
-      ],
-      imports: [HttpModule]
+        BasketProductService
+      ]
     });
   }));
-
-  describe('#getAllProducts()', () => {
-
-    it('should return all messages',
-      async(inject([ MockBackend, BasketProductService ], (mockBackend, service) => {
-        mockBackend.connections.subscribe(
-          (connection: MockConnection) => {
-            connection.mockRespond(new Response(
-              new ResponseOptions({
-                body: { '_embedded': { 'products':
-                  [ { title : 'ProductTest1' }, { title : 'TitleTest2' } ] } }
-
-              })
-            ));
-            connection.mockError(new ResponseError());
-          });
-
-        service.getAllProducts().subscribe((data) => {
-          expect(data.length).toBe(2);
-          expect(data[0].product.title).toBe('TitleTest1');
-          expect(data[1].product.title).toBe('TitleTest2');
-        });
-      })));
-  });
 
   it('should ...', inject([BasketProductService], (service: BasketProductService) => {
     expect(service).toBeTruthy();
--- src/app/basketProduct/basketProduct.service.ts
+++ src/app/basketProduct/basketProduct.service.ts
@@ -10,39 +10,13 @@ export class BasketProductService {
 
   products: BasketProduct[] = [];
 
-  constructor (private http: Http,
-               private authentication: AuthenticationBasicService) { }
+  constructor () { }
 
-  // GET /products
-  getAllProducts(): Observable<BasketProduct[]>{
-    return this.http.get(`${environment.API}/basketProducts`)
-      .map((res: Response) => res.json()._embedded.basketProducts)
-      .catch((error: any) => Observable.throw(error.json()));
-  }
-  addProduct(product: BasketProduct): Observable<BasketProduct> {
-    let body = JSON.stringify({
-      'product': product.product
-    });
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    headers.append('Authorization', this.authentication.getCurrentUser().authorization);
-    let options = new RequestOptions({ headers: headers });
-    this.products.push(product);
-    console.log(this.products[0].product);
-    return this.http.post(`${environment.API}/basketProducts`, body, options)
-      .map((res: Response) => res.json())
-      .catch((error: any) => Observable.throw(error.json()));
-  }
-  getArrayProducts(): BasketProduct[]{
+  getAllProducts(): BasketProduct[] {
     return this.products;
   }
-  // DELETE /product/:id
-  deleteProductByUri(uri: string) {
-    let headers = new Headers({ 'Authorization': this.authentication.getCurrentUser().authorization });
-    let options = new RequestOptions({ headers: headers });
 
-    return this.http.delete(`${environment.API}${uri}`, options)
-      .map((res: Response) => res.ok)
-      .catch((error: any) => Observable.throw(error.json()));
+  addProduct(product: BasketProduct): void {
+    this.products.push(product);
   }
-
 }

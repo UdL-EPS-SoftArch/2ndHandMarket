bcc0e2bf4ab4f478953d6dabe5d8c8cb3f01571c
Roberto García <roberto@rhizomik.net>
2016-12-22 10:56:16 +0100

    Picture service returns instances of Picture

--- src/app/advertisement/picture/picture.ts
+++ src/app/advertisement/picture/picture.ts
@@ -1,10 +1,14 @@
 export class Picture {
-  uri: string = '';
-  filename: string = '';
-  content: string = '';
+  uri: string;
+  filename: string;
+  content: string;
   depicts: string;
 
   constructor(values: Object = {}) {
     (<any>Object).assign(this, values);
+  }
+
+  getUriId(): number {
+    return this.uri ? Number(this.uri.split('/')[2]) : null;
   }
 }

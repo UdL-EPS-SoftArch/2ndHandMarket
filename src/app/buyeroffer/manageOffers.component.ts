56c7aeb215842200509cce55648e3b3a462a2f0b
xavier <xplanes123@gmail.com>
2016-12-25 12:54:35 +0100

    Function to submit offers implemented, ready for the handler and hide the offers if the product is already sold

--- src/app/buyeroffer/manageOffers.component.ts
+++ src/app/buyeroffer/manageOffers.component.ts
@@ -1,5 +1,5 @@
 /**
- * Created by ierathenz on 26/11/16.
+ * Created by Xavier on 14/12/16.
  */
 import { Component, OnInit } from '@angular/core';
 import { BuyerOffer } from './buyeroffer';
@@ -7,24 +7,30 @@ import { BuyerOfferService } from './buyeroffer.service';
 import { AdvertisementService } from '../advertisement/advertisement.service';
 import { Advertisement } from '../advertisement/advertisement';
 import { AuthenticationBasicService } from '../login-basic/authentication-basic.service';
+import {PurchaseService} from "../purchase/purchase.service";
+import {Purchase} from "../purchase/purchase";
 
 
 @Component({
   selector: 'app-manage-offers',
   templateUrl: './manageOffers.component.html',
   styleUrls: ['./manageOffers.component.css'],
-  providers: [BuyerOfferService, AdvertisementService]
+  providers: [BuyerOfferService, AdvertisementService, PurchaseService]
 })
 export class ManageOffersComponent implements OnInit {
 
   buyeroffers: BuyerOffer[] = [];
   errorMessage: string;
   newBuyerOffer: BuyerOffer = new BuyerOffer();
+  newPurchase: Purchase = new Purchase();
   advertisements: Advertisement[] = [];
+  hasPurchased: boolean = false;
+  advertisement: Advertisement;
 
   constructor(private buyerofferService: BuyerOfferService,
               private advertisementService: AdvertisementService,
-              private authentication: AuthenticationBasicService) { }
+              private authentication: AuthenticationBasicService,
+              private purchase: PurchaseService) { }
 
   ngOnInit() {
     this.getBuyerOffer();
@@ -43,6 +49,18 @@ export class ManageOffersComponent implements OnInit {
         },
         error => this.errorMessage = <any>error.message
       );
+  }
+
+  submitOfferAndPurchase(offer : BuyerOffer, advert : Advertisement){
+    advert.price = offer.value;
+    this.newPurchase = new Purchase({ advert, });
+    this.purchase.addPurchase(this.newPurchase).subscribe(
+      purchase => {
+        this.newPurchase = purchase;
+        this.hasPurchased = true;
+      },
+      error => this.errorMessage = error.message
+    );
   }
 
   getBuyerOffer() {

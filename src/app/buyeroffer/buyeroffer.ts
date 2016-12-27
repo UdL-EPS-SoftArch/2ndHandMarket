d2d82ba3be57f02ebe3f4f5ee0ec19d1e47e5e7d
julio77nz <julioqr94@gmail.com>
2016-12-28 00:41:45 +0100

    Advertiseent link with uri, instead of all attributes.

--- src/app/buyeroffer/buyeroffer.ts
+++ src/app/buyeroffer/buyeroffer.ts
@@ -2,6 +2,7 @@ import {Advertisement} from "../advertisement/advertisement";
 
 export class BuyerOffer {
   uri: string = '';
+  advOffers: string;
   advertisement: string;
   buyer_id: number;
   advertisement_id: number;

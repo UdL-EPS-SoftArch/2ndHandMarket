89d4aedeb8239a79745f1ae0eb0663919f7de049
Roberto García <roberto@rhizomik.net>
2017-01-03 18:57:31 +0100

    Upgrade to angular-cli 1.0.0-beta.24 (angular 2.3.1)

--- src/app/auth0/auth0.module.ts
+++ src/app/auth0/auth0.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AUTH_PROVIDERS } from 'angular2-jwt';
 import { Auth0Component } from './auth0.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
-  providers: [AUTH_PROVIDERS],
+  providers: [],
   declarations: [Auth0Component],
   exports: [Auth0Component]
 })

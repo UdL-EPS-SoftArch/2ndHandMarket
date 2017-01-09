b2eeef45d135c240bbb93876f2135b34d205b057
julio77nz <julioqr94@gmail.com>
2017-01-09 09:42:48 -0800

    Now Update Works fine (update all attributes of the BuyerOffer fine).

--- src/app/buyeroffer/updateOffer.component.ts
+++ src/app/buyeroffer/updateOffer.component.ts
@@ -37,6 +37,10 @@ export class UpdateOfferComponent implements OnInit {
   updateOffer(existingOffer: BuyerOffer, newPrice: number) {
     let updatedOffer: BuyerOffer = existingOffer;
     updatedOffer.value = newPrice;
+    updatedOffer.advertisement_id = existingOffer.advertisement_id;
+    updatedOffer.advertisement_title = existingOffer.advertisement_title;
+    updatedOffer.advertisement_seller = existingOffer.advertisement_seller;
+    updatedOffer.advertisement_iniPrice = existingOffer.advertisement_iniPrice;
     this.buyerofferService.updateOfferById(existingOffer.uri, updatedOffer)
       .subscribe(
         update => this.buyeroffers = this.buyeroffers.map(buyerOffer => {

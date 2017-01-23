43e35360068dd1b16f8731b30b1875231ff0f521
zurfyx <zurfyx@gmail.com>
2017-01-23 19:05:06 +0100

    closes #91 - display search related terms as user types

--- src/app/advertisement/search-advertisement/search-advertisement.component.ts
+++ src/app/advertisement/search-advertisement/search-advertisement.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Advertisement } from '../advertisement';
+import { SearchAdvertisementService } from './searchAdvertisement.service';
 
 @Component({
   selector: 'app-search-ad',
   templateUrl: './search-advertisement.component.html',
   styleUrls: ['./search-advertisement.component.scss'],
+  providers: [ SearchAdvertisementService ],
 })
 
 export class SearchAdvertisementComponent implements OnInit {
@@ -14,13 +16,29 @@ export class SearchAdvertisementComponent implements OnInit {
   isDropdownVisible: boolean;
   searchInput: string = '';
 
-  constructor(private router: Router) {
+  MAX_MATCHING_ADVERTISEMENTS = 5;
+  matchingAdvertisements: Advertisement[] = [] ;
+
+  constructor(private router: Router, private searchAdvertisement: SearchAdvertisementService) {
   }
 
   ngOnInit() {}
 
-  submitSearch() {
+  inputChange() {
+    if (this.searchInput.length < 1) {
+      return;
+    }
+
+    this.searchAdvertisement.searchAdvertisementByTitle(this.searchInput)
+      .subscribe(
+        advertisements => { this.matchingAdvertisements = advertisements.slice(0, this.MAX_MATCHING_ADVERTISEMENTS); },
+        error => console.error('Error retrieving advertisements'),
+      );
+  }
+
+  submitSearch(term) {
     this.isDropdownVisible = false;
+    this.searchInput = term || this.searchInput;
     this.router.navigate(['/'], { queryParams: { title: this.searchInput } });
   }
 

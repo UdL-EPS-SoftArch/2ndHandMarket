f1f4b7a092db0bc705d58620a183f3c0b032c77c
zurfyx <zurfyx@gmail.com>
2017-01-23 18:26:38 +0100

    show / hide search-advertisement dropdown

--- src/app/advertisement/search-advertisement/search-advertisement.component.ts
+++ src/app/advertisement/search-advertisement/search-advertisement.component.ts
@@ -6,12 +6,12 @@ import { Advertisement } from '../advertisement';
 @Component({
   selector: 'app-search-ad',
   templateUrl: './search-advertisement.component.html',
-  styleUrls: ['./search-advertisement.component.scss']
+  styleUrls: ['./search-advertisement.component.scss'],
 })
 
 export class SearchAdvertisementComponent implements OnInit {
 
-  advertisements: Advertisement[] = [];
+  isDropdownVisible: boolean;
   searchInput: string = '';
 
   constructor(private router: Router) {
@@ -20,6 +20,7 @@ export class SearchAdvertisementComponent implements OnInit {
   ngOnInit() {}
 
   submitSearch() {
+    this.isDropdownVisible = false;
     this.router.navigate(['/'], { queryParams: { title: this.searchInput } });
   }
 

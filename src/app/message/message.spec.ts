f2dd66e1685e6fa50b724b6a1dff02df82c46f39
Jap9 <jap9@alumnes.udl.cat>
2017-01-24 21:07:34 +0100

    Added Message tests on message.spec.ts

--- src/app/message/message.spec.ts
+++ src/app/message/message.spec.ts
@@ -7,4 +7,29 @@ describe('Message', () => {
   it('should create an instance', () => {
     expect(new Message()).toBeTruthy();
   });
+
+  it('should accept values in the constructor', () => {
+    const uri = "/privateMessages/5";
+    const title = "TestMessage";
+    const body = "TestBody";
+    const destination = "TestDestination";
+    const sender = "TestSender";
+    const isRead = true;
+
+    let _message = new Message({
+      uri: uri,
+      title: title,
+      body: body,
+      destination: destination,
+      sender: sender,
+      isRead: isRead,
+    });
+
+    expect(_message.uri).toEqual(uri);
+    expect(_message.title).toEqual(title);
+    expect(_message.body).toEqual(body);
+    expect(_message.destination).toEqual(destination);
+    expect(_message.sender).toEqual(sender);
+    expect(_message.isRead).toEqual(isRead);
+  });
 });
